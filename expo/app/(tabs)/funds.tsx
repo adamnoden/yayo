9936dbe930835a4171897496c9173773bfd64e78
Adam <107278274+adamnoden@users.noreply.github.com>
2024-04-07 14:18:10 +0100

    reset fund screen on navigate

--- expo/app/(tabs)/funds.tsx
+++ expo/app/(tabs)/funds.tsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { FundsOverview } from "../screens/FundsOverview";
 import { FundDetails } from "../screens/FundDetails";
+import { useFocusEffect } from "@react-navigation/native";
 
 const Funds: React.FC = () => {
   const [selected, setSelected] = useState<string | null>(null);
+
+  useFocusEffect(
+    useCallback(() => {
+      // called when the tab gains focus
+
+      return () => {
+        // called when the tab goes out of focus
+        resetSelection();
+      };
+    }, [])
+  );
+
   const selectFund = (fundId: string) => {
     console.log(fundId);
     setSelected(fundId);
